9869f17806fe321f2def1df1ffae541246a7d2c0
Jimcl6 <jedllorente@gmail.com>
2023-08-02 23:02:50 +0800

    Exploring DOM learning about the window and document global variable

---
+++ app.js
@@ -0,0 +1,11 @@
+// console.log(window);
+
+// window global variable gives us access to information and functionality to the active brower's tab/window
+// console.log(document);
+
+// document global variable:- Like the "window" global variable, this variable gives us access to information and functionality to the
+// content displayed on our browser's tab / window. 
+// to view the javascript object we use the console.dir() method. as this .dir() method displays an interactive list of the properties
+// of the specified Javascript object, and this overrides the .log() method.
+
+console.dir(document);
