4bf6cec5dccd7eaada4f66d960b2bb10737597b3
Jimcl6 <jedllorente@gmail.com>
2023-08-11 22:53:46 +0800

    learned to use querySelector

--- dom.js
+++ dom.js
@@ -7,13 +7,12 @@
 // above syntax is a method known as query selecting:- Selecting elements does not require us full knowledge of our DOM Structure
 // We can select our HTML elements in a way that is similar to selecting elements in our CSS files.
 
-
-let anchorElement = document.getElementById('external-link');
-anchorElement.href = 'https://www.google.com';
-
+let anchorElement = document.getElementById("external-link");
+anchorElement.href = "https://www.google.com";
 
 // .querySelector() - this method returns the first element that is a descendant of a node that matches selectors.
 // like CSS we can use CSS selectors to call the ID names and Class names of our HTML Element.
 // we can also use the "html element tag" to select the HTML tags that we want to manipulate.
-anchorElement = document.querySelector('p');
+anchorElement = document.querySelector("p a");
 // anchorElement.outerText = 'Hello World';
+anchorElement.href = "https://academind.com";
