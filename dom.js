ab9e583143378189fd153b3caee32256c61c5803
Jimcl6 <jedllorente@gmail.com>
2023-08-13 11:20:08 +0800

    learned how I can work with innerHTML property

--- dom.js
+++ dom.js
@@ -26,7 +26,8 @@ newAnchorElement.href = "https://www.google.com";
 // 2. Get access to the parent element that should hold the new element.
 
 let firstParagraph = document.querySelector("p");
-
+firstParagraph.textContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 // 3. Insert the new element into the parent element content.
 
 firstParagraph.appendChild(newAnchorElement);
@@ -43,3 +44,11 @@ selectedElement.remove(); // this syntax can be used in modern day browsers.
 
 // ANOTHER WAY OF REMOVING AN HTML ELEMENT
 // selectedElement.parentElement.removeChild(selectedElement); // this syntax can only be used on older browsers.
+
+// MOVING ELEMENTS
+
+// firstParagraph.parentElement.append(firstParagraph);
+
+// WORKING W/ INNER HTML
+
+firstParagraph.innerHTML = "This is <strong>Max!</strong>";
